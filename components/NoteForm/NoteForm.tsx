8ad07ef86d85b49c1100af29f98a00d5986d6a2c
Yuliia <ciceron131313@gmail.com>
2026-02-14 15:29:04 +0200

    Update NoteForm.tsx

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -2,28 +2,14 @@
 import css from "../NoteForm/NoteForm.module.css"
 import { useId } from "react";
 import { useMutation, useQueryClient} from '@tanstack/react-query';
-import * as Yup from "yup";
 import {createNote} from "../../lib/api"
-import {ErrorMessage} from "formik";
-import Loader from "../Loader/loader";
 import { useNoteDraftStore } from '@/lib/stores/noteStore';
+import Loader from "@/app/loading"
 
 interface NoteFormProps {
-
  onClose: () => void;
 }
 
-interface FormValues {
-  title: string,
-  content: string,
-  tag: string,
-};
-
-const initialValues: FormValues = {
-  title: "",
-  content: "",
-  tag: "Todo",
-};
 
 export default function NoteForm ({onClose}: NoteFormProps) {
 const queryClient = useQueryClient();
@@ -35,7 +21,6 @@ const { draft, setDraft, clearDraft } = useNoteDraftStore();
       HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
     >,
   ) => {
-	  // 4. Коли користувач змінює будь-яке поле форми — оновлюємо стан
     setDraft({
       ...draft,
       [event.target.name]: event.target.value,
@@ -49,20 +34,16 @@ const {mutate, isPending} = useMutation({
     queryClient.invalidateQueries({queryKey: ["notes"]});
     onClose();
   },
-  onError: () => {
-
-  },
 });
 
 
-const validationSchema = Yup.object().shape({
-  title: Yup.string().min(3).max(50).required("Title is required"),
-  content: Yup.string().max(500 , "Max length is 500"),
-  tag: Yup.string().required(),
-});
-
-const handleSubmit = (values: FormValues) => {
-  mutate(values);
+const handleSubmit = (formData: FormData) => {
+    const values = Object.fromEntries(formData) as {
+      title: string;
+      content: string;
+      tag: string;
+    };
+      mutate(values);
 };
 
 return (
@@ -77,41 +58,37 @@ return (
         Content
         <textarea name="content" defaultValue={draft?.content} onChange={handleChange}></textarea>
       </label>
-
-    <ErrorMessage name="title" component="p" className={css.error}
-    />
-  </form>
-
-  <div className={css.formGroup}>
+        <div className={css.formGroup}>
     <label htmlFor="content">Content</label>
-    <input as="textarea"
+    <textarea
       id="content"
       name="content"
       rows={8}
       className={css.textarea}
       defaultValue={draft?.title} onChange={handleChange}
     />
-     <ErrorMessage name="content" component="p" className={css.error}/>
   </div>
 
   <div className={css.formGroup}>
     <label htmlFor="tag">Tag</label>
-    <input as="select" id={`${fieldId}-tag`} name="tag" className={css.select} defaultValue={draft?.title} onChange={handleChange}>
+    <select id={`${fieldId}-tag`} name="tag" className={css.select} defaultValue={draft?.title} onChange={handleChange}>
       <option value="Todo">Todo</option>
       <option value="Work">Work</option>
       <option value="Personal">Personal</option>
       <option value="Meeting">Meeting</option>
       <option value="Shopping">Shopping</option>
-    </input>
-     <ErrorMessage name="tag" component="p" className={css.error}/>
+    </select>
   </div>
 
   <div className={css.actions}>
     <button type="button" className={css.cancelButton} onClick={onClose}> Cancel</button>
     <button type="submit" className={css.submitButton} disabled={isPending}>
-     {isPending ? <Loader/> : "Create note" }
+      {isPending ? <Loader/> : "Create note"}
     </button>
   </div>
+
+  </form>
+
   </>
   
   )}
