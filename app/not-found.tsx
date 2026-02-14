6ee21a92b6a45087ea154dba22d32c501f9fa746
Yuliia <ciceron131313@gmail.com>
2026-02-15 00:31:15 +0200

    k

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,41 +3,21 @@ import css from "./page.module.css"
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
-import type { Metadata } from "next";
 
-export const metadata: Metadata = {
-        title: "Type Error 404",
-        description: "Page is not find here 404!",
-     openGraph: {
-        title: `Type Error`,
-        description: "Page is not find here . Code with Type Error 404",
-        url: `https://notehub.com/notes`,
-        siteName: 'NoteHub',
-        images: [
-          {
-            url: 'https://ac.goit.global/fullstack/react/og-meta.jpg',
-            width: 1200,
-            height: 630,
-            alt: "Note Hub",
-          },
-        ],
-        type: 'article',
-      },
-    }
- 
 
 const NotFound = () => {
 
     const router = useRouter();
 
     useEffect(() => {
+        document.title = "404 | NoteHub";
         const timer = setTimeout(() => router.push('/'), 3000);
         return () => clearTimeout(timer);
     }, [router]);
     return (
         <div>
       <h1 className={css.title}>404 - Page not found</h1>
-<p className={css.description}>Sorry, the page you are looking for does not exist.</p>
+        <p className={css.description}>Sorry, the page you are looking for does not exist.</p>
       <Link href="/">Go back home</Link>
     </div>
     );
